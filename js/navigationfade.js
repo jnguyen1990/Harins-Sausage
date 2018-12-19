146aa80e13512011d0bb09574c387fe41210e74c
jnguyen1990 <joeanguyen1990@gmail.com>
2018-12-18 20:41:08 -0500

    fix colors and layout, add media

--- js/navigationfade.js
+++ js/navigationfade.js
@@ -1,23 +1,23 @@
-jQuery(function($) {
+// jQuery(function($) {
 
-  var $nav = $('#nav');
-  var $win = $(window);
-  var winH = $win.height()/10*7; // Get the window height.
+//   var $nav = $('#nav');
+//   var $win = $(window);
+//   var winH = $win.height()/10*7; // Get the window height.
 
-  $win.on("scroll", function() {
-    if ($(this).scrollTop() > winH - 81) {
-      $nav.addClass("bluenav");
-      $nav.addClass("bluenavtext");
-      $nav.removeClass("whitenav");
-      $nav.removeClass("whitenavtext");
-    } else {
-      $nav.addClass("whitenav");
-      $nav.addClass("whitenavtext");
-      $nav.removeClass("bluenav");
-      $nav.removeClass("bluenavtext");
-    }
-  }).on("resize", function() { // If the user resizes the window
-    winH = $(this).height()/10*7; // you'll need the new height value
-  });
+//   $win.on("scroll", function() {
+//     if ($(this).scrollTop() > winH - 81) {
+//       $nav.removeClass("darknav");
+//       $nav.removeClass("darknavtext");
+//       $nav.addClass("whitenav");
+//       $nav.addClass("whitenavtext");
+//     } else {
+//       $nav.removeClass("whitenav");
+//       $nav.removeClass("whitenavtext");
+//       $nav.addClass("darknav");
+//       $nav.addClass("darknavtext");
+//     }
+//   }).on("resize", function() { // If the user resizes the window
+//     winH = $(this).height()/10*7; // you'll need the new height value
+//   });
 
-});
+// });
